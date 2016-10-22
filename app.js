e27c24fd70879317694599372e71e72d991f7453
王信凱 <jcskyting@gmail.com>
2016-10-22 12:06:15 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: "368a34d9-de33-4034-9180-a4640cee90f9",
+    appPassword: "zLRa1dPBcYqv7oiAdHDsqzg"
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
